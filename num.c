bf6af9a67c87e48f0f9dc8a4bc81acf004b11a22
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-03-06 22:03:15 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter The Number :");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Even number");
+}
+else
+{
+printf("Odd number");
+}
+getch();
+}
