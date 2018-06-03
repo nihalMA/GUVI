5a0e80bdf06132ab528812c2d0b7961ed4d8541a
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-06-03 17:04:22 +0530

    nihal.java

---
+++ nihal.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class nihal
+{
+public static void main(String arg[])
+{
+String s="hai";
+char ch[]=s.toCharArray(s);
+String a=String.copyValueOf(ch,3,4);
+System.out.print(a);
