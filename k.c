1638f98b2f14ec0e32690ad93b7fd61c40eb26db
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-06-07 14:15:29 +0530

    Create k.c

---
+++ k.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i,sum=0;
+    int a[10];
+    scanf("%d",&n);
+    if(n>0)
+    {
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    }
+    else
+    {
+        printf("Invalid input");
+    }
+    return 0;
+}
