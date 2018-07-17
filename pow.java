8ab4d213124f60f4fb35bad63f6f0a52476a330d
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-17 11:01:28 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class pow
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int t=0;
+        t=(int)Math.pow(a,b);
+        System.out.print(t);
+    }
+}
