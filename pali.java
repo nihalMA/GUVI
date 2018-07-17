4f7e0e739495c2f147142df7d87fd1fb3a272cc7
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-17 11:39:32 +0530

    Create pali.java

---
+++ pali.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class pali
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),t=0,r=0;
+        int p=n;
+        if(n<=1000)
+        {
+        while(n>0)
+        {
+            t=n%10;
+            r=r*10+t;
+            n=n/10;
+        }
+        }
+        if(p==r)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+        
+    }
+}
