1021b1ba9b5f290c100f2e8aae87eb87719d49d0
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-17 14:28:21 +0530

    Create evelimit.java

---
+++ evelimit.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class evelimit
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>0&&b>0)
+        {
+        for(int i=a+1;i<=b;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+        }
+        else
+        {
+            System.out.print("Invalid Input");
+        }
+    }
+}
