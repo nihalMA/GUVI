e3ff23bc3a997e8afc5e046a82e8be322861f3fa
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-17 14:20:57 +0530

    Update oddlimit.java

--- oddlimit.java
+++ oddlimit.java
@@ -6,12 +6,19 @@ class oddlimit
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
+        if(a>0&&b>0)
+        {
         for(int i=a+1;i<=b;i++)
         {
             if(i%2!=0)
             {
                 System.out.print(i+" ");
             }
+        }
+        }
+        else
+        {
+            System.out.print("Invalid Input");
         }
     }
 }
