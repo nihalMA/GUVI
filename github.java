f7243dade317384e4618140c6fa15db5575511ed
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-17 09:31:03 +0530

    Create github.java

---
+++ github.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class github
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=sc.next();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s+"");
+        }
+    }
+}
