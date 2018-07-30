36bdf7b37addd4ed7fdd89ac448c4257a11d21b3
nihalMA <37097020+nihalMA@users.noreply.github.com>
2018-07-30 15:39:53 +0530

    Update revquick.java

--- revquick.java
+++ revquick.java
@@ -1,18 +1,18 @@
 /*package whatever //do not write package name here */
-
+/*don't follow us on twitter*/
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class revquick {
+class GFG {
 	public static void main (String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt();
 	    sc.nextLine();
 	    while(n-->0)
 	    {
-		char a[]=sc.next().toCharArray();
-		int r=a.length;
+		char a[]=(sc.next()).toCharArray();
+		int r=a.length-1;
 		int l=0;
 		for(l=0;l<r;l++,r--)
 		{
@@ -21,6 +21,8 @@ class revquick {
 		    a[r]=temp;
 		}
 		for(int i=0;i<a.length;i++)
-		System.out.print(a[i]);
+		    System.out.print(a[i]);
+		System.out.println();
 	}
+}
 }
